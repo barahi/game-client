4eb558404558f02a69d2bbdb5e9f3047d35f7701
swornimbarahi <swornimbarahi@gmail.com>
2021-05-05 22:13:28 -0400

    Added Switch and Router to App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import './App.css';
-import Boxes from "./components/Box"
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Boxes from "./components/Box";
+
+const App = () => {
   return (
-    <div className="App">
-      <Boxes />
-      <Boxes />
-      <Boxes />
-      <Boxes />
-      <Boxes />
-      <Boxes />
-      <Boxes />
-      <Boxes />
-      <Boxes />
-    </div>
+    <Router>    
+      <div className="App">
+        <Link to='/foo'>Go to foo</Link>
+        <br />
+        <Link to='/bar'>Go to bar</Link>
+        <Switch>
+          <Route path='/foo' component={Foo} />
+          <Route path='/bar' component={Bar} />
+        </Switch>
+        <Boxes />
+        <Boxes />
+        <Boxes />
+        <Boxes />
+        <Boxes />
+        <Boxes />
+        <Boxes />
+        <Boxes />
+        <Boxes />
+      </div>
+    </Router>
   );
+}
+
+const Foo = () => {
+  return <div>Foo</div>
+}
+
+const Bar = () => {
+  return <div>Bar</div>
 }
 
 export default App;
